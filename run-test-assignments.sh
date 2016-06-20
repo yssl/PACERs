311dd25fd2759e42d76c27583b4f72492ed3a1ac
Yoonsang Lee <yslee111@gmail.com>
2016-06-21 07:50:00 +0900

    Add test scripts for test-assignments

---
+++ run-test-assignments.sh
@@ -0,0 +1,6 @@
+./pacers.py test-assignments/c-assignment-1
+./pacers.py test-assignments/c-assignment-2 --user-input "1 2" "3 4"
+./pacers.py test-assignments/c-assignment-3 --user-dict "{'1':[''], '2':['2 5', '10 20']}"
+./pacers.py test-assignments/c-assignment-4 --user-dict "{'1':[''], '2':['2 5', '10 20']}"
+
+./pacers.py test-assignments/txt-assignment
