6be342a5ef76ceee21800287ece6274b9b2b8eb7
Yoonsang Lee <yslee111@gmail.com>
2019-12-09 17:55:16 +0900

    Process \t between add_executable( and the first argument

--- pacerslib/run.py
+++ pacerslib/run.py
@@ -115,7 +115,7 @@ def runcmd_cmake(srcRootDir, projName):
     buildDir = opjoin(srcRootDir, gBuildDirPrefix+projName)
     execName = projName
     with open(opjoin(srcRootDir,'CMakeLists.txt'), 'r') as f:
-        tokens = re.split(' |\n|\(|\)', f.read())
+        tokens = re.split(' |\t|\n|\(|\)', f.read())
         tokens = [x for x in tokens if x]   # remove empty strings
         for i in range(len(tokens)):
             if tokens[i].lower()=='add_executable' and i < len(tokens)-1:
